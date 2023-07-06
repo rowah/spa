d1af38c8a980f91685e53e98a2c1f0d2b5b9eae7
Rowa <jrowah@gmail.com>
2023-07-06 23:10:29 +0300

    improve ui by rendering component if no articles to be listed

---
+++ lib/spa_web/components/empty_state.ex
@@ -0,0 +1,21 @@
+defmodule SpaWeb.EmptyState do
+  # tell elixir that this is a component
+  use SpaWeb, :html
+
+  attr(:text, :string, required: true)
+  attr(:image, :string, required: true)
+
+  def show(assigns) do
+    ~H"""
+    <div>
+      <h2 class="text-lg font-semibold tracking-tight sm:text-center sm:text-2xl"><%= @text %></h2>
+      <div class="mt-10 mx-auto w-full max-w-xs">
+        <img
+          src={~p"/images/#{@image}"}
+          class="w-full max-w-none rounded-xl ring-1 ring-gray-400/10 md:ml-4 lg:-ml-0"
+        />
+      </div>
+    </div>
+    """
+  end
+end

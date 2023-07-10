63a5bfe43c993fb405415a957e74153140db34c0
Rowa <jrowah@gmail.com>
2023-07-10 20:57:42 +0300

    add articles

--- lib/spa_web/components/empty_state.ex
+++ lib/spa_web/components/empty_state.ex
@@ -11,7 +11,7 @@ defmodule SpaWeb.EmptyState do
       <h2 class="text-lg font-semibold tracking-tight sm:text-center sm:text-2xl"><%= @text %></h2>
       <div class="mt-10 mx-auto w-full max-w-xs">
         <img
-          src={~p"/images/#{@image}"}
+          src={~p"/images/cooking/#{@image}"}
           class="w-full max-w-none rounded-xl ring-1 ring-gray-400/10 md:ml-4 lg:-ml-0"
         />
       </div>
